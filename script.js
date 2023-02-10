bd8fb95d83c4c94e54b75c7d1938316a59a4238f
RChipman <rschipman@gmail.com>
2023-02-10 20:41:13 -0300

    if else

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hi HI!");
+console.log("before Conditional");
+if (1 + 2 === 2) {
+  console.log("Math still works");
+} else {
+  console.log("Math out of order");
+}
+console.log("After Conditional");
