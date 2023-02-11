e6b638dfe8e167afed4e46e75b7ca636e650a719
RChipman <rschipman@gmail.com>
2023-02-11 15:42:32 -0300

    prompt and else if statments

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-console.log("hi HI!");
-console.log("before Conditional");
-if (1 + 2 === 2) {
-  console.log("Math still works");
+// console.log("hi HI!");
+// console.log("before Conditional");
+// if (1 + 2 === 2) {
+//   console.log("Math still works");
+// } else {
+//   console.log("Math out of order");
+// }
+// console.log("After Conditional");
+
+const password = prompt("please enter new password");
+
+if (password.length >= 6) {
+  console.log("LONG ENOUGH PASSWORD!");
 } else {
-  console.log("Math out of order");
+  console.log("PASSWORD TOO SHORT! Must be 6+ characters");
 }
-console.log("After Conditional");
