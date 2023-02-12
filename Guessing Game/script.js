31d6b83439bfb1e7efb709a9d9d3e408b3d7116f
RChipman <rschipman@gmail.com>
2023-02-12 13:17:45 -0300

    Guessing Game Fin

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -1,3 +1,26 @@
 let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+  maximum = parseInt(prompt("Enter a valid number!"));
+}
 
-Math.floor(Math.random() * 10) + 1;
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "quit") break;
+  attempts++;
+  if (guess > targetNum) {
+    guess = prompt("Too high! Guess again!");
+  } else {
+    guess = prompt("Too low! Guess again!");
+  }
+}
+if (guess === "quit") {
+  console.log(`Game over! The number was: ${targetNum}`);
+} else {
+  console.log(
+    `You got it! It took you ${attempts} guesses. The number was ${targetNum}`
+  );
+}
